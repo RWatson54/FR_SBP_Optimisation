7753d55458d8e58307c23a4043a2bd0111bf493d
Rob Watson <r.a.watson@lboro.ac.uk>
2022-05-26 17:22:07 +0100

    Adding in a few new polynomial basis functions (2D Euclidean and 2D Total)

--- optimisePoints/getBasisFunctions.m
+++ optimisePoints/getBasisFunctions.m
@@ -29,9 +29,28 @@ switch basisType
 
         end
 
+    case 'Total2D'
+
+        % -- For the 2D total order basis, the requested number of bases should be a triangular number
+        isTriangular = mod(sqrt(8*nBasis+1),1)==0;
+        if ~isTriangular
+
+            error('Incompatible number of bases requested for a Total2D basis')
+
+        end
+
+    case 'Euclid2D'
+
+        % -- For the Euclidean basis, there's no easy closed form for the test, so list the numbers (OEIS A000603)
+        isTriangular = ismember(nBasis,[1 3 6 11 17 26 35 45 58 73 90 106 123 146 168 193 216 243]);
+        if ~isTriangular
+
+            error('Incompatible number of bases requested for a Total2D basis')
+
+        end
     otherwise
 
-        error('No corresponding trial basis functions set. Goodbye.\n')
+        error('No corresponding trial basis functions set in that size. Goodbye.\n')
 
 end
 
@@ -40,9 +59,19 @@ switch basisType
 
     case 'Maximal2D'
 
-        % -- For the 2D maximal order basis, the requested number of bases should be a square number
+        % -- Set the 2D Maximal Order basis polynomial function handle
         basisO = @(x,d) maximalOrder2D(x,d,nBasis);
 
+    case 'Total2D'
+
+        % -- Set the 2D Total Order basis polynomial function handle
+        basisO = @(x,d) totalOrder2D(x,d,nBasis);
+
+    case 'Euclid2D'
+
+        % -- Set the 2D Euclidean Order basis polynomial function handle
+        basisO = @(x,d) euclidOrder2D(x,d,nBasis);
+            
     otherwise
 
         error('No corresponding trial basis functions set. Goodbye.\n')
@@ -109,3 +138,86 @@ function [b] = maximalOrder2D(x,d,n)
 
 end
 
+% -- Function for two dimensional total order basis
+function [b] = totalOrder2D(x,d,n)
+
+    % -------------------------------------------------------------
+
+    % -- The inputs are:
+
+    %       x -- the coordinates to evaluate the basis at [nCoord -x- nDim]
+    %       d -- the direction in which to differentiate, if any
+    %       n -- the number of bases to compute
+
+    % -- The outputs are:
+
+    %       b -- the basis evaluated at the coordinate points
+
+    % -------------------------------------------------------------
+
+    % -- Get the corresponding triangular number
+    nOTotalOrder = (round(sqrt(8*n+1)) - 1) / 2;
+
+    % -- Get the maximal powers involved
+    nOMaxOrder = nOTotalOrder;
+    pow1D = 0:1:nOMaxOrder-1;
+    xPowM = reshape(repmat(pow1D',1,nOMaxOrder),[],1);
+    yPowM = reshape(repmat(pow1D ,nOMaxOrder,1),[],1);
+
+    % -- Truncate the maximal powers to get just the total order basis
+    xPow = xPowM(xPowM + yPowM <= (nOTotalOrder-1));
+    yPow = yPowM(xPowM + yPowM <= (nOTotalOrder-1));   
+
+    % -- Compute the basis
+    if d == 0
+        b = (x(:,1)'.^(xPow) .* x(:,2)'.^(yPow));
+    elseif d == 1   
+        b = (max(0,xPow) .* x(:,1)'.^(max(0,xPow-1)) .* x(:,2)'.^(yPow));   
+    elseif d == 2
+        b = (x(:,1)'.^(xPow) .* max(0,yPow) .* x(:,2)'.^(max(0,yPow-1)));
+    end
+
+end
+
+% -- Function for two dimensional Euclidean order basis
+function [b] = euclidOrder2D(x,d,n)
+
+    % -------------------------------------------------------------
+
+    % -- The inputs are:
+
+    %       x -- the coordinates to evaluate the basis at [nCoord -x- nDim]
+    %       d -- the direction in which to differentiate, if any
+    %       n -- the number of bases to compute
+
+    % -- The outputs are:
+
+    %       b -- the basis evaluated at the coordinate points
+
+    % -------------------------------------------------------------
+
+    % -- Get the corresponding triangular number
+
+    % -- No easy closed form for this, so use find
+    nOEuclidOrder = find([1 3 6 11 17 26 35 45 58 73 90 106 123 146 168 193 216 243] == n);
+    
+    % -- Get the maximal powers involved
+    nOMaxOrder = nOEuclidOrder;
+    pow1D = 0:1:nOMaxOrder-1;
+    xPowM = reshape(repmat(pow1D',1,nOMaxOrder),[],1);
+    yPowM = reshape(repmat(pow1D ,nOMaxOrder,1),[],1);
+
+    % -- Truncate the maximal powers to get just the total order basis
+    xPow = xPowM(xPowM.^2 + yPowM.^2 <= (nOEuclidOrder-1)^2);
+    yPow = yPowM(xPowM.^2 + yPowM.^2 <= (nOEuclidOrder-1)^2);   
+
+    % -- Compute the basis
+    if d == 0
+        b = (x(:,1)'.^(xPow) .* x(:,2)'.^(yPow));
+    elseif d == 1   
+        b = (max(0,xPow) .* x(:,1)'.^(max(0,xPow-1)) .* x(:,2)'.^(yPow));   
+    elseif d == 2
+        b = (x(:,1)'.^(xPow) .* max(0,yPow) .* x(:,2)'.^(max(0,yPow-1)));
+    end
+
+end
